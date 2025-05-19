b50f9067d334cdc69a67aac7925f1937c5aa1b57
aryan110011 <thakursadhvi19@gmail.com>
2025-05-19 15:45:54 +0530

    Update serfu.py

--- serfu.py
+++ serfu.py
@@ -7,7 +7,7 @@ import sys
 
 init(autoreset=True)
 
-SERVER_URL = "https://server-evn1.onrender.com"
+SERVER_URL = "https://sarfu.onrender.com"
 GITHUB_APPROVAL_URL = "https://raw.githubusercontent.com/aryan110011/sarfu/main/aproval.txt"
 
 # Stylized Red Logo
